c17c716752bc941bf48da6d136d26c7b0c01bbd0
Usman Rasheed Siddiqui - Navigator <siddiqui.uas1@gmail.com>
2024-11-06 21:55:56 +0500

    Display DataBase added and Database Entries edited

--- Databases Entries.py
+++ Databases Entries.py
@@ -14,18 +14,20 @@ lines = text.split("\n")
 print(lines)
 databases.close()
 
+
 # For allotting numbers to each database:
 
 no_databases = []
 for num in range(1, len(lines)+1):
     no_databases.append(num)
-print(no_databases)
+print("No. of database:", no_databases)
 
 # Access a Data Base:
 
 open_database = int(input("Enter the number of database you want to open: "))
 
-databases = open("DataBases.txt")
-for i, line in range(1, len(lines)+1), databases:
+open_db = lines[open_database - 1]
+print(open_db)
 
-    existing_database = open(line.strip())
+with open(f"{open_db}",'w') as file:
+    file.write("I exist!")  # User Input in file
---
+++ Display Database.py
@@ -0,0 +1,77 @@
+# For Displaying DataBase:
+
+
+display_database = input("Enter name of database you want to open: ")  # DataBase .txt file name
+
+    #   Fetching System Files:
+with open(f"System Files/{display_database}.txt") as system_database:
+    for line in system_database:
+        if line.strip() == "Tables":        # Tables
+            tables = eval(system_database.readline())
+            print("Tables:", tables)
+        elif line.strip() == "Fields & Lengths":    # Fields & Lengths
+            fields_lengths = eval(system_database.readline())
+            print("Fields & Lengths:", fields_lengths)
+        elif line.strip() == "Primary Keys":        # Primary Keys
+            primary_keys = eval(system_database.readline())
+            print("Primary Keys:", primary_keys)
+
+    #   Fetching Data Files:
+with open(f"Data Files/{display_database}.txt") as existing_database:
+        name_record = input("Enter name of record: ")  # Enter Record Name
+
+        for line in existing_database:
+            if line.strip() == name_record:  # Heading Line
+                if name_record not in tables:   #   Finding tables in system database
+                    print("Table Not Found")
+                else:
+                    database_dict = existing_database.readline()  # Access Dictionary Line
+                    database_dict = eval(database_dict)  # Record to dictionary
+                    print(f"{name_record}:", database_dict)
+
+#   Displaying Records:
+
+def display(name_record, field_lengths, primary_keys):
+    if name_record not in fields_lengths:
+        print(f"Tables {name_record} not found")
+        return
+
+    # Fetching Field_Lengths
+
+    field_lengths = field_lengths[name_record]
+    field_names = list(field_lengths.keys())
+
+    # Display the field names as headings:
+
+    header =''
+    for field in field_names:
+        header += f"{field:{field_lengths[field]}} |"
+    print(header.strip())
+
+    # Display Records (values of each field):
+    for record_key in database_dict:
+        record_values = database_dict[record_key]
+        row=''
+
+        j = 0
+        for values in record_values:
+            row += f'{str(values):{field_lengths[field_names[j]]}} |'
+            j += 1
+        print(row.strip())
+
+display(name_record, fields_lengths, primary_keys)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
