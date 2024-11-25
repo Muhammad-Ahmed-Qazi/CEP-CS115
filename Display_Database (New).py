b7f720be9d4a178add5fd7b2c6648cfc3152a448
Usman Rasheed Siddiqui - Navigator <siddiqui.uas1@gmail.com>
2024-11-26 00:14:58 +0500

    Display Selected Database added. Do check it plz. It's fully functional

--- Display_Database (New).py
+++ Display_Database (New).py
@@ -1,12 +1,13 @@
+from re import search
 
 #Extracting Files from file_handler
 from file_handler import get_fields, get_records
+from data_manipulation import search_record
 
 db_name = input("Enter name of database you want to open: ")   # Entering Name of database
 table = input("Enter name of the table you want to view: ")             #Entering name of the table to view
 
 #   Displaying Records:
-print(f'{table}: ')         # Printing record name
 
 def display(db_name, table):
     '''This function is being used to fetch fields, their lengths and the respective records from the table
@@ -40,7 +41,6 @@ def display(db_name, table):
             continue
         record_values = record.split(', ')  # Split the record into its individual values
 
-
     #   Fetching Each record one by one:
         row = ''
         for values, field in zip(record_values, range(len(field_names))):
@@ -60,7 +60,40 @@ display(db_name, table)
 
 #help(display_database)
 
+def selected_database(db_name, table):
+
+    record = search_record(db_name, table).strip().split(",")
+    fields = get_fields(db_name)[table]
+
+    field_lengths = list(fields.values())               # Fetching field lengths
+    field_names = list(fields.keys())                   # Fetching field names
+
+    bold_italic = '\033[1m\033[3m'      # ANSI escape code for bold and italics
+    reset = '\033[0m'               # ANSI escape code to reset the formatting
+
+    # Creating a header string to display field names (column titles)
+    header = ''
+    for field in range(len(field_lengths)):
+        # Adjusting the width of the display of respective fields based on the lengths and formatting using bold_italics
+        header += f"| {bold_italic}{field_names[field]:{field_lengths[field]}}{reset} |"    # Displaying each heading separated by pipelines
+    print(header.strip())                                             #   Removing whitespaces and newline commands
+
+    if record:
+        # Creating a row to display record's values in a neat and formatted way
+        row = ''
+        for values, field in zip(record, range(len(field_names))):
+
+        # To determine appropriate spaces and alignment of pipes from top to bottom
+            if len(field_names[field]) > field_lengths[field]:
+                col_length = len(field_names[field])        # If field length is longer, use that length
+            else:
+                col_length = field_lengths[field]           # Otherwise, use the provided length
 
 
+            row += f'| {str(values):{col_length}} |'      # To display each record separated by pipes
 
+        # Display the formatted row for the record
+        print(row.strip())
+
+selected_database(db_name, table)
 
