c971a1f7e021c9fab8d51b9e61e9cd9d962727ca
Muhammad-Ahmed-Qazi <qmuhammadahmed@gmail.com>
2024-11-26 08:06:15 +0500

    Addition of comments to project files in Terminal App folder.

--- Terminal App/data_manipulation.py
+++ Terminal App/data_manipulation.py
@@ -21,20 +21,21 @@ def add_record(db_name, table):
     # Getting input for each field of the table
     print(f"\nEnter data for respective fields in '{table}':")
     while True:
-        data = input(f"{pk}: ")
-        if 0 < len(data) <= fields[pk]:
-            pk_entry = data
+        data = input(f"{pk}: ")      # Input for primary key
+        if 0 < len(data) <= fields[pk]: # Ensure the data length is within the defined limit
+            pk_entry = data     # Assign the input as the primary key entry
             break
         else:
-            print(f"Input must have at least 1 and maximum {fields[pk]} characters!")
-    if record_exists(db_name, table, data):
-        return 0
+            print(f"Input must have at least 1 and maximum {fields[pk]} characters!")   # Prompt for valid input instruction
+    if record_exists(db_name, table, data):     # Check if a record with the primary key already exists
+        return 0    # if the record already exists
 
+    # Getting input for each field (except the primary key) of the table
     for field in fields:
-        if field != pk:
+        if field != pk:     #Skip the primary key
             while True:
-                data = input(f"{field}: ")
-                if 0 < len(data) <= fields[field]:
+                data = input(f"{field}: ")      # Input for other fields
+                if 0 < len(data) <= fields[field]:  # Ensure the data length is within the defined limit
                     record.append(data)
                     break
                 else:
@@ -42,13 +43,14 @@ def add_record(db_name, table):
         else:
             record.append(pk_entry)
 
-    write_record(db_name, table, format_data(record))
+    write_record(db_name, table, format_data(record))   # Write the formatted record to the table file
     return 1
 
 # Formatting record entries into a string to be written to the data files
 def format_data(record):
-    data_info = ""
+    data_info = ""      # Initialize a  empty string to hold formatted data
 
+    # Concatenate each field
     for field in record:
         data_info += field + ', '
 
@@ -83,58 +85,57 @@ def format_data(record):
 def edit_record(db_name, table):
     '''This function enables user to edit a record in selected database user entered.'''
 
-    fields = get_fields(db_name)[table]  # Getting fields
+    fields = get_fields(db_name)[table]  # Getting fields for the table
+    field_names = list(fields.keys())  # Getting field names list
+    fetch_record = search_record(db_name, table)    # Search for the record to be edited
 
-    field_names = list(fields.keys())  # Getting field names
+    record = fetch_record.strip().split(",")    # Split the record into individual field values
 
-    fetch_record = search_record(db_name, table)
-
-    record = fetch_record.strip().split(",")
-
-     #Ensure that the record has the correct number of fields
+     # Ensure that the record has the correct number of fields
     if len(record) != len(field_names):
-        print(f"Error: Following Record does not exist!")
+        print(f"Error: Following Record does not exist!")    # Record does not exist or has an incorrect format
         return
 
-
-    if record:                                                                  # Verifying that the record is not an empty list
-        for i, field in zip(range(len(field_names)), field_names):              # Proceed to editing the record
+    if record:           # Verifying that the record is not an empty list
+        for i, field in zip(range(len(field_names)), field_names):        # Proceed to editing the record
 
             while True:
-                edit_record = input(f"Edit new record for {field}: ")               # Entering new value for the field
+                edit_record = input(f"Edit new record for {field}: ")      # Entering new value for the field
                 print()
-                print(f"Editing field '{field}' with value: {edit_record}")
+                print(f"Editing field '{field}' with value: {edit_record}")     # Show the editing action
                 print()
-                if 0 < len(edit_record) <= fields[field]:                       # Checking length of the input
-                    record[i] = edit_record                                     # Update the record at the correct index
+                # Ensure the length of the new value is valid for the field
+                if 0 < len(edit_record) <= fields[field]:
+                    record[i] = edit_record                    # Update the record at the correct index
                     break
 
                 else:
-                    print(f"The length of the input must be from 1 character to {fields[field]} characters")
+                    print(f"The length of the input must be from 1 character to {fields[field]} characters")    # Prompt for valid input instruction
 
     else:
-        print("Following record does not exist")
+        print("Following record does not exist")    # If no record is found, display error message
         return
 
 
     # Format the edited record for writing
     formatted_edit = format_data(record)
 
+    # Update the record in the table file
     update_records(db_name, table, fetch_record, formatted_edit)
 
-    print("Record successfully updated")
+    print("Record successfully updated")    # Confirmation message
 
 # Delete a record using the primary key input by the user
 def delete_record(db_name, table, record):
     if record:
-        update_records(db_name, table, record)
-        return 1
+        update_records(db_name, table, record)  # Remove the record from the table
+        return 1    # Indicates the record was successfully deleted
     else:
-        return 0
+        return 0    # If the record does not exist
 
 # Searching whether a record with the primary key input exists in the database or not
 def search_record(db_name, table):
-    pk = get_primary_keys(db_name)[table]
+    pk = get_primary_keys(db_name)[table]   # Get the primary key for the table
 
-    pk_data = input(f"\nEnter the value for '{pk}' to get the record: ")
-    return record_exists(db_name, table, pk_data)
+    pk_data = input(f"\nEnter the value for '{pk}' to get the record: ")    # primary key value from the user
+    return record_exists(db_name, table, pk_data)   # Check if a record with the given primary key exists
--- Terminal App/display.py
+++ Terminal App/display.py
@@ -1,9 +1,13 @@
+# Importing necessary functions from different modules:
 from database_setup import create_database
 from data_manipulation import add_record, search_record, delete_record, edit_record
 from file_handler import get_db_names, get_tables, get_fields, get_records
 
+# Main menu function which provides user with initial options
 def main_menu():
     while True:
+
+        # Displaying Main Menu options
         print("\n=== Simple Database Management System ===")
         print("1. Create a New Database")
         print("2. Open Existing Database")
@@ -12,9 +16,11 @@ def main_menu():
         # Get user choice
         choice = input("Select an option (1-3): ")
 
+        # Option 1: Create Database:
         if choice == '1':
             if create_database():
                 print("\n\033[92mDatabase created successfully!\033[0m")
+                # Displaying Steps to add records to the newly created database
                 print(
                     "\nTo add records, follow these steps:\n"
                     "\t1. Return to the main menu and select 'Open Existing Database.'\n"
@@ -23,26 +29,38 @@ def main_menu():
                     "This will allow you to start entering records into your table."
                 )
             else:
+                # If database already exists, prompt an error message
                 print("\n\033[91mDatabase already exists with this name!\033[0m")
+
+        # Option 2: Opening an existing database
         elif choice == '2':
+            # If no database exist, display an error message
             if len(get_db_names()) == 0:
                 print("\n\033[91mNo databases exist! Please create one first!\033[0m")
             else:
                 open_database()
+
+        # Option 3: Exit the program
         elif choice == '3':
             print("\n\033[94mExiting the program. Goodbye!\033[0m")
             break
+
+        # Invalid choice error prompt
         else:
             print("\n\033[91mInvalid option. Please select a valid option (1-3).\033[0m")
 
+# Function to open an existing database and provide options for further manipulation
 def open_database():
-    db_names = get_db_names()
+    db_names = get_db_names()           # Getting databases' names
 
     # Selecting database to open
     db_name = show_list("database", db_names)
 
     while True:
-        tables = get_tables(db_name)
+
+        tables = get_tables(db_name)                # Getting list of tables
+
+        # Displaying options to perform manipulation of user's choice
         print(f"\n--- Database: {db_name} ---\n")
         print("1. Add a Record")
         print("2. Edit a Record")
@@ -51,76 +69,125 @@ def open_database():
         print("5. Display All Records in a Table")
         print("6. Return to Main Menu")
 
+        # Taking input from the user for manipulation
         choice = input("Select an option (1-6): ")
 
-        # Add a record
+        # Option 1: Add a record
         if choice == '1':
+
+            # Adding record and checking if it already exists (on the basis of primary keys)
             if not add_record(db_name, show_list("table", tables)):
                 print("\nRecord with this primary key already exists!")
             else:
                 print("\nRecord successfully added!")
 
+        # Option 2: Edit a Record
         elif choice == '2':
+            # Editing record from the selected table
             edit_record(db_name, show_list("table", tables))
 
-        # Delete a record
+        # Option 3: Delete a record
         elif choice == '3':
+            # Deleting a record by selecting a table and searching for it
             table = show_list("table", tables)
             if delete_record(db_name, table, search_record(db_name, table)):
                 print("\nRecord successfully deleted!")
             else:
                 print("\nRecord with this primary key does not exist!")
 
-        # Search for and Display a record
+        # Option 4: Search for and Display a record
         elif choice == '4':
             display_selected(db_name, show_list("table", tables))
 
-        # Display all records in a table
+        # Option 5: Display all records in a table
         elif choice == '5':
             display_all(db_name, show_list("table", tables))
 
-        # Returning to main menu
+        # Option 6: Returning to main menu
         elif choice == '6':
             print("\nReturning to main menu.")
             break
 
         else:
-            print("\nInvalid option. Please select a valid option (1-6).")
+            print("\nInvalid option. Please select a valid option (1-6).")      # In case of invalid input
 
+# Function to print list of tables in the database the user opened
+# through the Terminal and take input from the user of which table the user wants to choose.
 def show_list(entities, info):
     print("\nChoose a table from the list below to perform the operation:")
     for i in range(len(info)):
         print(f"{i + 1}. {info[i]}")
 
     while True:
+        # Handling selection when there is only one option
         if len(info) == 1:
             entity = input(f"Select {entities} (1): ")
         else:
+            # Handling selection when there are multiple options
             entity = input(f"Select {entities} (1-{len(info)}): ")
         try:
+            # Returning the selected entity
             entity = info[int(entity) - 1]
             return entity
         except IndexError:
+            # Prompting message for invalid input
             print(f"Invalid option. Please select a valid option (1-{len(info)})." if len(info) != 1 else f"Invalid option. Please select a valid option (1).")
 
-def display_selected(db_name, table):
-    record = search_record(db_name, table).split(",")
-    header = "|"
-    data = "|"
+# Muhammad Ahmed Qazi
+# def display_selected(db_name, table):
+#     record = search_record(db_name, table).split(",")
+#     header = "|"
+#     data = "|"
 
-    if record != ['']:
-        fields = get_fields(db_name)[table]
-        for field, entry in zip(fields, record):
-            if len(entry) > len(field):
-                col_length = len(entry) + 2
+#     if record != ['']:
+#         fields = get_fields(db_name)[table]
+#         for field, entry in zip(fields, record):
+#             if len(entry) > len(field):
+#                 col_length = len(entry) + 2
+#             else:
+#                 col_length = len(field) + 2
+#             header += f" \033[1m\033[3m{field.strip():{col_length}}\033[0m |"
+#             data += f" {entry.strip():{col_length}} |"
+#         print("\n" + header)
+#         print(data)
+#     else:
+#         print("\nRecord with this primary key entry does not exist.")
+
+# Usman Rasheed Siddiqui
+def display_selected(db_name, table):
+
+    record = search_record(db_name, table).strip().split(",")
+    fields = get_fields(db_name)[table]
+
+    field_lengths = list(fields.values())               # Fetching field lengths
+    field_names = list(fields.keys())                   # Fetching field names
+
+    bold_italic = '\033[1m\033[3m'      # ANSI escape code for bold and italics
+    reset = '\033[0m'               # ANSI escape code to reset the formatting
+
+    # Creating a header string to display field names (column titles)
+    header = ''
+    for field in range(len(field_lengths)):
+        # Adjusting the width of the display of respective fields based on the lengths and formatting using bold_italics
+        header += f"| {bold_italic}{field_names[field]:{field_lengths[field]}}{reset} |"    # Displaying each heading separated by pipelines
+    print(header.strip())                                             #   Removing whitespaces and newline commands
+
+    if record:
+        # Creating a row to display record's values in a neat and formatted way
+        row = ''
+        for values, field in zip(record, range(len(field_names))):
+
+        # To determine appropriate spaces and alignment of pipes from top to bottom
+            if len(field_names[field]) > field_lengths[field]:
+                col_length = len(field_names[field])        # If field length is longer, use that length
             else:
-                col_length = len(field) + 2
-            header += f" \033[1m\033[3m{field.strip():{col_length}}\033[0m |"
-            data += f" {entry.strip():{col_length}} |"
-        print("\n" + header)
-        print(data)
-    else:
-        print("\nRecord with this primary key entry does not exist.")
+                col_length = field_lengths[field]           # Otherwise, use the provided length
+
+
+            row += f'| {str(values):{col_length}} |'      # To display each record separated by pipes
+
+        # Display the formatted row for the record
+        print(row.strip())
 
 # Muhammad Ahmed Qazi
 # def display_all(db_name, table):
@@ -154,22 +221,25 @@ def display_all(db_name, table):
     in order to view them when the user gives the appropriate prompt'''
 
 
-    fields = get_fields(db_name)[table]        # Fetching fields
-    records = get_records(db_name, table)      # Fetching records
+    # Fetching fields and their respective lengths from table
+    fields = get_fields(db_name)[table]        # Fetching fields for the given table
+    records = get_records(db_name, table)      # Fetching all records in the table
 
 
-    # Fetching Field Lengths And Names
+    # Fetching Field Lengths And Names from fetched fields
 
     field_lengths = list(fields.values())               # Fetching field lengths
     field_names = list(fields.keys())                   # Fetching field names
 
     # Display the field names as headings:
     # For headings to be Bold And Italic
-    bold_italic = '\033[1m\033[3m'
-    reset = '\033[0m'
+    bold_italic = '\033[1m\033[3m'      # ANSI escape code for bold and italics
+    reset = '\033[0m'               # ANSI escape code to reset the formatting
 
+    # Creating a header string to display field names (column titles)
     header = ''
     for field in range(len(field_lengths)):
+        # Adjusting the width of the display of respective fields based on the lengths and formatting using bold_italics
         header += f"{bold_italic}{field_names[field]:{field_lengths[field]}}{reset} |"    # Displaying each heading separated by pipelines
     print(header.strip())                                             #   Removing whitespaces and newline commands
 
@@ -182,16 +252,18 @@ def display_all(db_name, table):
         record_values = record.split(', ')  # Split the record into its individual values
 
 
-    #   Fetching Each record one by one:
+        # Creating a row to display each record's values in a neat and formatted way
         row = ''
         for values, field in zip(record_values, range(len(field_names))):
 
         # To determine appropriate spaces and alignment of pipes from top to bottom
             if len(field_names[field]) > field_lengths[field]:
-                col_length = len(field_names[field])
+                col_length = len(field_names[field])        # If field length is longer, use that length
             else:
-                col_length = field_lengths[field]
+                col_length = field_lengths[field]           # Otherwise, use the provided length
+
 
             row += f'{str(values):{col_length}} |'      # To display each record separated by pipes
 
+        # Display the formatted row for this record
         print(row.strip())
